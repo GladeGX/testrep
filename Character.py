ae6f2e0759175207d7a5e325bd0bfee0028b4942
GladeGX <vit858vit@gmail.com>
2024-11-02 17:25:21 +0300

    Update Character.py

--- Character.py
+++ Character.py
@@ -7,18 +7,37 @@ class Character():
 
     def take_item(self,item):
         self.inventory.append(item)
-        print(f'Ты взял {item}')
+        print(f'{self.name} взял {item}')
     def drop_item(self,item):
         if item in self.inventory:
             self.inventory.remove(item)
-            print(f'Ты выбросил {item}')
+            print(f'{self.name} выбросил {item}')
         else:
-            print(f'У тебя нет {item}')
+            print(f'У {self.name} нет {item}')
 
     def attack(self,other):
         dmg = self.strenght
         other.health -= dmg
         print(f'{self.name} нанес {other.name} {dmg} урона! У {other.name} осталось {other.health} HP')
+
+class Mage(Character):
+    def __int__(self,name,health,strenght,mana):
+        super().__init__(self,name,health,strenght)
+        self.mana = mana
+
+    def cast_navik(self,minus_mana):
+        self.mana -= minus_mana
+        print(f'{self.name} использует заклинание, расходуя {minus_mana} маны (Остал. {self.mana})')
+
+class Varvor(Character):
+    def __int__(self,name,health,strenght,yarost):
+        super().__init__(self,name,health,strenght)
+        self.yarost = yarost
+
+    def attack(self,yarost,other):
+        dmg = self.strenght
+        other.health -= dmg * yarost
+        print(f'{self.name} нанес {other.name} {dmg} урона! (Бонус {yarost}!). У {other} осталось {other.health} HP')
 
 
 
@@ -29,10 +48,10 @@ class Item():
         self.name = name
         self.value = value
 
-chelik = Character('Ayato',100,15)
-other = Character('Raiden',50,40)
-sword = Item('sword',20)
+person = Character('Аято',100,15)
+person2 = Character('Райден',50,40)
+mage = Mage('Эмилия',70,40,100)
+varvor = Varvor('Итто',150,30)
 
-chelik.take_item('rassekayushiy tuman')
-chelik.drop_item('rassekayushiy tuman')
-chelik.attack(other)
+
+#sword = Item('sword',20)
